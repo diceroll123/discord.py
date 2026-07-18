0fcc8d9ab46dbf595cac2255ffa71076f0c354a1
Steve C <diceroll123@gmail.com>
2026-07-18 19:09:46 -0400

    fix: change is_spoiler to only use flag
    Mimicking advaith's https://github.com/discordjs/discord.js/pull/11560

--- discord/message.py
+++ discord/message.py
@@ -277,8 +277,7 @@ class Attachment(Hashable):
 
     def is_spoiler(self) -> bool:
         """:class:`bool`: Whether this attachment contains a spoiler."""
-        # The flag is technically always present but no harm to check both
-        return self.filename.startswith('SPOILER_') or self.flags.spoiler
+        return self.flags.spoiler
 
     def is_voice_message(self) -> bool:
         """:class:`bool`: Whether this attachment is a voice message."""
